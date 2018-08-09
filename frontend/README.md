88e3909cac2bc166b51b9426d5f8b5676f75ddeb
Noah Huppert <contact@noahh.io>
2018-08-08 23:55:58 -0400

    wrote basic react 'hello world' app

--- frontend/README.md
+++
@@ -1,6 +0,0 @@
-# frontend
-
-An absolute bare-bones web app.
-
-Created from templates made available by Stagehand under a BSD-style
-[license](https://github.com/dart-lang/stagehand/blob/master/LICENSE).
---
+++ frontend/webpack.config.js
@@ -0,0 +1,45 @@
+var path = require("path");
+
+var webpack = require("webpack");
+
+var CleanWebpackPlugin = require("clean-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// Constants
+const buildDir = path.join(__dirname, "dist");
+
+// Configuration
+module.exports = {
+	mode: "development",
+	devtool: "source-map",
+	entry: [
+		"./src/index.jsx"
+	],
+	output: {
+		path: buildDir,
+		filename: "[name].js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)/,
+				exclude: /node_modules/,
+				use: ["babel-loader"]
+			},
+			{
+				test: /\.scss/,
+				use: ["style-loader", "css-loader", "sass-loader"]
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin([buildDir]),
+		new HtmlWebpackPlugin({
+			title: "GH Gantt",
+			meta: {
+				viewport: "width=device-with, initial-scale=1"
+			},
+			template: "src/index.html"
+		})
+	]
+};
