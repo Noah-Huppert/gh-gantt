ec5a1349cddd7a9c46ba23e156b2325aeb93e257
Noah Huppert <contact@noahh.io>
2018-08-26 17:37:18 -0400

    wrote http responder interface

---
+++ server/http/responder.go
@@ -0,0 +1,11 @@
+package http
+
+import (
+	"net/http"
+)
+
+// Responder provides an extendible way to respond to HTTP requests
+type Responder interface {
+	// Respond sends a response to an HTTP request
+	Respond(w http.ResponseWriter, r *http.Request)
+}
