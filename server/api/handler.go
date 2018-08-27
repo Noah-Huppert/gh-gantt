50beefe21a40e16c369145e9cf48a62e58f7c8ba
Noah Huppert <contact@noahh.io>
2018-08-26 21:04:10 -0400

    added health check endpoint to api handler setup function

--- server/api/handler.go
+++ server/api/handler.go
@@ -1,18 +1,10 @@
 package api
 
 import (
-	"fmt"
-	"net/http"
+	"github.com/gorilla/mux"
 )
 
-// NewAPIHandler creates a new http.Handler to serve API requests
-func NewAPIHandler() http.Handler {
-	// Create mux
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "OK")
-	})
-
-	return mux
+// SetupRouter registers API routes with the provided router
+func SetupRouter(router *mux.Router) {
+	router.Handle("/healthz", NewHealthCheckHandler()).Methods("GET")
 }
