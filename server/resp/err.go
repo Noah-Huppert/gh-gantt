f657370dbaa36e3af92db4894b2286ebb42aeee8
Noah Huppert <contact@noahh.io>
2018-08-27 10:48:45 -0400

    made error responder take more detailed err info

--- server/resp/err.go
+++ server/resp/err.go
@@ -1,30 +1,67 @@
 package resp
 
 import (
+	"errors"
 	"net/http"
+
+	"github.com/Noah-Huppert/golog"
 )
 
 // ErrorResponder implements the Responder interface by sending an error to the client
 type ErrorResponder struct {
-	// Error is the error to return to the user
-	Error error
+	// logger is used to display the DetailError
+	logger golog.Logger
 
 	// Status is the HTTP status code to set when sending the error
 	Status int
+
+	// PublicError is the error to return to the user
+	PublicError error
+
+	// DetailError provides more details error information which will not be shown to the end user. Can be nil
+	DetailError error
 }
 
 // NewErrorResponder creates an ErrorResponder
-func NewErrorResponder(err error, status int) ErrorResponder {
+func NewErrorResponder(logger golog.Logger, status int, pubErr error, detErr error) ErrorResponder {
 	return ErrorResponder{
-		Error:  err,
-		Status: status,
+		logger:      logger,
+		Status:      status,
+		PublicError: pubErr,
+		DetailError: detErr,
+	}
+}
+
+// NewStrErrorResponder creates an ErrorResponder. An error for PublicError and DetailError will be created from the
+// error string arguments.
+func NewStrErrorResponder(logger golog.Logger, status int, pubErrStr, detErrStr string) ErrorResponder {
+	var detErr error = nil
+	if len(detErrStr) > 0 {
+		detErr = errors.New(detErrStr)
+	}
+
+	return ErrorResponder{
+		logger:      logger,
+		Status:      status,
+		PublicError: errors.New(pubErrStr),
+		DetailError: detErr,
 	}
 }
 
 // Respond implements Responder.Respond
 func (e ErrorResponder) Respond(w http.ResponseWriter, r *http.Request) {
+	if e.PublicError == nil {
+		panic("ErrorResponder.PublicError can not be nil")
+	}
+
+	if e.DetailError != nil {
+		e.logger.Errorf("%s: %s", e.PublicError.Error(), e.DetailError.Error())
+	} else {
+		e.logger.Error(e.PublicError.Error())
+	}
+
 	resp := map[string]string{
-		"error": e.Error.Error(),
+		"error": e.PublicError.Error(),
 	}
 
 	jsonResponder := NewJSONResponder(resp, e.Status)
