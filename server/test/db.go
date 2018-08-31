19ae4dcc3d960fb6b1612df8159d86da179aa722
Noah Huppert <contact@noahh.io>
2018-08-30 23:56:28 -0400

    wrote helper to connect to test db

---
+++ server/test/db.go
@@ -0,0 +1,20 @@
+package test
+
+import (
+	"github.com/Noah-Huppert/gh-gantt/server/config"
+	"github.com/Noah-Huppert/gh-gantt/server/db"
+
+	"github.com/jmoiron/sqlx"
+)
+
+// ConnTestDB connects to the database
+func ConnTestDB() (*sqlx.DB, error) {
+	dbCfg := DBConfig{
+		DBHost:     "localhost",
+		DBPort:     5432,
+		DBName:     "test-gh-gantt",
+		DBUsername: "test-gh-gantt",
+	}
+
+	return db.Connect(dbCfg)
+}
