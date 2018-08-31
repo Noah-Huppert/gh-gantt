9da324e50894d42c5fef16a58c9427c00ae486da
Noah Huppert <contact@noahh.io>
2018-08-30 23:52:29 -0400

    documented db.sh script behavior more

--- server/scripts/db.sh
+++ server/scripts/db.sh
@@ -7,9 +7,10 @@
 #
 # ARGUMENTS
 #	1. ENV    (Optional) Environment to start local server for. Value cannot be "prod". Defaults to "dev".
-
-# Configuration
-db_data_dir="$PWD/$(dirname $0)/../run-data"
+#
+# BEHAVIOR
+#	Starts a local PostgreSQL server with a user and database named ENV-gh-gantt
+#	Saves data in the run-data/ENV directory.
 
 # Load arguments
 db_env="$1"
@@ -22,17 +23,21 @@ if [[ "$db_env" == "prod" ]]; then
 	exit 1
 fi
 
-# Run
+# Configuration
+db_data_dir="$PWD/$(dirname $0)/../run-data/$db_env"
 db_name="$db_env-gh-gantt"
 db_username="$db_env-gh-gantt"
 
 echo "#######################"
 echo "# Starting PostgreSQL #"
 echo "#######################"
-echo "Env        : $db_env"
-echo "DB Name    : $db_name"
-echo "DB Username: $db_username"
+echo "Env           : $db_env"
+echo "Data directory: $db_data_dir"
+echo "DB Name       : $db_name"
+echo "DB Username   : $db_username"
 
+# Run
+mkdir -p "$db_data"
 docker run \
 	-it \
 	--rm \
