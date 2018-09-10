40d5573c1c87ace5b68213297f28a6d7bbdb57c1
Noah Huppert <contact@noahh.io>
2018-09-10 16:06:22 -0400

    server: wrote connect method for sqlx.DB

--- server/libdb/conn.go
+++ server/libdb/conn.go
@@ -12,6 +12,7 @@ import (
 
 // Connect to a PostgreSQL database
 func Connect(dbCfg config.DBConfig) (*sql.DB, error) {
+	// Assemble database connection string
 	sqlConnStr := fmt.Sprintf("host=%s port=%d dbname=%s user=%s sslmode=disable", dbCfg.DBHost, dbCfg.DBPort,
 		dbCfg.DBName, dbCfg.DBUsername)
 
@@ -19,10 +20,25 @@ func Connect(dbCfg config.DBConfig) (*sql.DB, error) {
 		sqlConnStr += fmt.Sprintf("password=%s", dbCfg.DBPassword)
 	}
 
+	// Connect to database
 	db, err := sql.Open("postgres", sqlConnStr)
 	if err != nil {
 		return nil, fmt.Errorf("error connecting to the database: %s", err.Error())
 	}
 
 	return db, nil
+}
+
+// ConnectX connects to a PostgreSQL database an then makes an sqlx.DB instance
+func ConnectX(dbCfg config.DBConfig) (*sqlx.DB, error) {
+	// Connect
+	db, err := Connect(dbCfg)
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to database: %s", err.Error())
+	}
+
+	// Make sqlx.DB instance
+	dbx := sqlx.NewDb(db, "postgres")
+
+	return dbx, nil
 }
