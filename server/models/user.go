20d130bbfdddc5b09daef23971a81d91d62423b5
Noah Huppert <contact@noahh.io>
2018-09-14 20:43:10 -0400

    server: wrote user query method

--- server/models/user.go
+++ server/models/user.go
@@ -1,5 +1,9 @@
 package models
 
+import (
+	"github.com/jmoiron/sqlx"
+)
+
 // User represents a GitHub user who has logged in to use the GH Gantt application
 type User struct {
 	// GitHubID is user's ID which was retrieved from the GitHub API
@@ -23,4 +27,11 @@ func NewUser(ghID, name, login, profilePicURL string) *User {
 		Login:             login,
 		ProfilePictureURL: profilePicURL,
 	}
+}
+
+// QueryByGitHubID searches the database for a User with a matching GitHubID.
+//
+// Returns an error if one occurs. Returns sql.ErrNoRows if a matching User is not found.
+func (u *User) QueryByGitHubID(db *sqlx.DB) error {
+	return db.Get(u, "SELECT name, login, profile_picture_url FROM users WHERE github_id = ?", u.GitHubID)
 }
