4b2d32b79895bc21442a105abc47c96df4713719
Noah Huppert <contact@noahh.io>
2018-09-23 22:28:10 -0400

    server: added user insert method

--- server/models/user.go
+++ server/models/user.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"fmt"
+
 	"github.com/jmoiron/sqlx"
 )
 
@@ -34,4 +36,28 @@ func NewUser(ghID, name, login, profilePicURL string) *User {
 // Returns an error if one occurs. Returns sql.ErrNoRows if a matching User is not found.
 func (u *User) QueryByGitHubID(db *sqlx.DB) error {
 	return db.Get(u, "SELECT name, login, profile_picture_url FROM users WHERE github_id = $1", u.GitHubID)
+}
+
+// Insert adds a new User row to the database.
+func (u *User) Insert(db *sqlx.DB) error {
+	// Start transaction
+	tx, err := db.Beginx()
+	if err != nil {
+		return fmt.Errorf("error starting transaction: %s", err.Error())
+	}
+
+	// Insert
+	err = tx.QueryRowx("INSERT INTO users (github_id, name, login, profile_picture_url) VALUES ($1, $2, $3, $4) "+
+		"RETURNING id", u.GitHubID, u.Name, u.Login, u.ProfilePictureURL).StructScan(u)
+	if err != nil {
+		return fmt.Errorf("error executing insert statement: %s", err.Error())
+	}
+
+	// Commit transaction
+	err = tx.Commit()
+	if err != nil {
+		return fmt.Errorf("error committing transaction: %s", err.Error())
+	}
+
+	return nil
 }
