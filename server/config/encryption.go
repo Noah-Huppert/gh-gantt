a6ccbb25dc4bd701899b1fc0ac194e86051aa671
Noah Huppert <contact@noahh.io>
2018-10-26 11:39:14 -0400

    server: removed uneeded encryption helpers

--- server/config/encryption.go
+++
@@ -1,7 +0,0 @@
-package config
-
-// EncryptionConfig holds symmetric encryption related configuration
-type EncryptionConfig struct {
-	// AuthTokenEncryptionSecret is the secret key used to symmetrically encrypt third party authentication tokens
-	AuthTokenEncryptionSecret string `required:"true" envconfig:"auth_token_encryption_secret"`
-}
