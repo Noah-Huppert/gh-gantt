663e8a31276b426bd7f6da39dff6770bb7e401de
Noah Huppert <contact@noahh.io>
2018-10-26 11:39:56 -0400

    server: removed uneeded gh auth token model

--- server/migrations/2_create_gh_auth_tokens_table.down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE github_authentication_tokens
--- server/migrations/2_create_gh_auth_tokens_table.up.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE github_authentication_tokens (
-	id SERIAL PRIMARY KEY,
-	github_user_id TEXT NOT NULL,
-	encrypted_access_token TEXT NOT NULL
-)
