0c8bed2aa3def128a684fd94ec92d747eb3058a1
Noah Huppert <contact@noahh.io>
2018-10-30 15:12:54 -0400

    frontend: started gh login page flow

--- frontend/src/components/ghLogin.jsx
+++ frontend/src/components/ghLogin.jsx
@@ -2,18 +2,64 @@ import React from "react"
 import PropTypes from "prop-types"
 import { connect } from "react-redux"
 
-/* Component */
-const GitHubLogin = ({ onClick, hello }) => (
-	<h1>Logging Into GitHub</h1>
-)
+/*
+ * Indicates what stage of authentication user is currently in.
+ * If the stage is empty REDIRECT_FROM_GH is assumed.
+ *
+ * Flows from one stage to another:
+ *
+ *	REDIRECT_FROM_GH
+ *    (check if `state` and `code` query params are present)
+ *        | [if present]      | [if not present]
+ *        |                   |
+ *        |                   >> BAD_REDIRECT_FROM_GH
+ *        v
+ *    (try exchanging `state` and `code` for auth token with GH Gantt API)
+ *        | [if success]      | [if failure]
+ *        |                   |
+ *        v                   >> FAILED_SERVER_EXCHANGE
+ *  GOOD_SERVER_EXCHANGE
+ *
+ */
+const GitHubAuthStage = {
+	REDIRECTED_FROM_GH: "REDIRECTED_FROM_GH", // User was just redirected from GitHub
+	BAD_REDIRECT_FROM_GH: "BAD_REDIRECT_FROM_GH", // GitHub redirect was malformed
+	FAILED_SERVER_EXCHANGE: "FAILED_SERVER_EXCHANGE", // Failed to exchange GitHub code for auth token
+	GOOD_SERVER_EXCHANGE: "GOOD_SERVER_EXCHANGE" // Successfully exchanged GitHub code for auth token
+}
 
-GitHubLogin.propTypes = {}
+/* Component */
+class GitHubLogin extends React.Component {
+	componentDidMount() {
+		if (this.stage === REDIRECT_FROM_GH || this.stage === undefined) {
+			// Just redirected from GitHub login
+			// Check for `state` and `code` query params
+			let params = new URLSearchParams(location.search)
+
+			if (params.get("state") === null || params.get("code") === null) {
+				// TODO: Dispatch BAD_REDIRECT_FROM_GH
+			}
+		}
+	}
+
+	render() {
+		return (
+			<h1>Logging Into GitHub</h1>
+		)
+	}	
+}
+
+GitHubLogin.propTypes = {
+	stage: PropTypes.string
+}
 
 export { GitHubLogin }
 
 /* Container */
 const mapStateToProps = state => {
-	return {}
+	return {
+		stage: state.auth.gh.stage
+	}
 }
 
 const mapDispatchToProps = dispatch => {
