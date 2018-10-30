a8f97d1b54bc8afcbf5883b5e9322c7b88341901
Noah Huppert <contact@noahh.io>
2018-10-29 23:53:31 -0400

    server: added gh state signing key to config

--- server/config/auth.go
+++ server/config/auth.go
@@ -4,4 +4,10 @@ package config
 type AuthConfig struct {
 	// SigningSecret is used to sign JWTs
 	SigningSecret string `required:"true" envconfig:"signing_secret"`
+
+	// GHStateSigningPrivKey is the ed25519 private key used to sign the state field in a GH authentication request
+	GHStateSigningPrivKey []byte `required:"true" envconfig:"gh_state_signing_priv_key"`
+
+	// GHStateSigningPubKey is the ed25519 public key used to sign the state field in a GH authentication request
+	GHStateSigningPubKey []byte `required:"true" envconfig:"gh_state_signing_pub_key"`
 }
