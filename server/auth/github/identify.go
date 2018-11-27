c89a3d09965422b614d244bc78e9e8860df7bcc1
Noah Huppert <contact@noahh.io>
2018-11-27 14:51:16 -0500

    server: wrote helper to identify gh user by auth token

---
+++ server/auth/github/identify.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+// Identify returns the user ID of the GitHub user who owns the provided GH auth token
+func Identify(ctx context.Context, authToken string) (string, error) {
+	// Create GitHub API client
+	tokenClient := oauth2.NewClient(ctx, oauth2.StaticTokenSource(&oauth2.Token{
+		AccessToken: authToken,
+	}))
+
+	client := github.NewClient(tokenClient)
+
+	// Make identify user request
+	user, _, err := client.Users.Get(ctx, "")
+	if err != nil {
+		return "", fmt.Errorf("error retrieving user information from GitHub API: %s", err.Error())
+	}
+
+	return strconv.FormatInt(user.ID, 10), nil
+}
