d5da85c33472cb97725d183505f17a85d01ba24e
Noah Huppert <contact@noahh.io>
2018-12-06 04:21:41 -0500

    frontend: made call auth zenhub append api endpoint

--- frontend/src/components/zenhubLoginPage.js
+++ frontend/src/components/zenhubLoginPage.js
@@ -1,18 +1,45 @@
+import { mapState } from "vuex"
+
+import { HomePageRoute } from "."
+
 export default {
 	data() {
 		return {
-			zenhubAuthToken: ""
+			zenhubAuthToken: "",
+			authOK: true
 		}
 	},
+	computed: mapState([
+		"authToken"
+	]),
 	template: `
 	<div>
-		<h1>Enter Your ZenHub Authentication Token</h1>
-		<input type="text" v-model="zenhubAuthToken" />
-		<button v-on:click="submit">Submit</button>
+		<div v-if="authOK">
+			<h1>Enter Your ZenHub Authentication Token</h1>
+			<input type="text" v-model="zenhubAuthToken" />
+			<button v-on:click="submit">Submit</button>
+		</div>
+
+		<div v-if="!authOK">
+			<h1>An Error Occurred</h1>
+		</div>
 	</div>
 	`,
 	methods: {
 		submit() {
+			var self = this;
+
+			api.authZenHubAppend(this.authToken, this.zenhubAuthToken)
+				.then(authToken => {
+					self.$store.commit("authToken", authToken)
+
+					router.push(HomePageRoute)
+					window.location.search = ""
+				})
+				.catch(err => {
+					console.error(err)
+					self.authOK = false
+				})
 		}
 	}
 }
